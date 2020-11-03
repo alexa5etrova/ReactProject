4657ef8c7ac57d20d0423ceecf3b9b60abff912b
Alexa <aleksa.5etrova@gmail.com>
2020-11-03 11:01:27 +0300

    ProfileInfo

---
+++ src/components/Profile/ProfileInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from "./ProfileInfo.modules.scss";
+
+const ProfileInfo =() =>{
+    return(
+        <div className={s.descriptionBlock}>
+            <div> ava + description </div>
+        </div>
+    )
+}
+
+export default ProfileInfo;
