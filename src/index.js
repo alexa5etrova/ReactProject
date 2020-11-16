ddc1cefc83e923c396af3818af10823cc7960cd7
unknown <aleksa.5etrova@gmail.com>
2020-11-16 16:27:53 +0300

    store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 
-import state from './redux/state';
+import store from './redux/state';
 import reportWebVitals from './reportWebVitals';
-import {renderEntireDoc} from './render';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import {addPost, updateNewPostText, updateNewMessageText, addMessage} from './redux/state';
 
 
+let renderEntireDoc = (state) => {
+    ReactDOM.render( 
+      <React.StrictMode>
+      <App 
+        state={state}
+        addPost={store.addPost.bind(store)}
+        addMessage={store.addMessage.bind(store)}
+        updateNewMessageText={store.updateNewMessageText.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        /> </React.StrictMode>,
+      document.getElementById('root')
+    );
+    
+  }
+  
 
-renderEntireDoc(state);
+renderEntireDoc(store.getState());
+
+store.subscribe(renderEntireDoc);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,61 +1,77 @@
-import {renderEntireDoc} from './../render';
-
-let state = {
-    profilePage: {
-         postData : [
-            {id: 1, message:"Finally, I've got a super rare gybrid", likes: "45"},
-            {id: 2, message:"Hi, there", likes: "5"}
-           ],
-        newPostText: 'Enter you post here'
+const store = {
+    _state: {
+        profilePage: {
+             postData : [
+                {id: 1, message:"Finally, I've got a super rare gybrid", likes: "45"},
+                {id: 2, message:"Hi, there", likes: "5"}
+               ],
+            newPostText: 'Enter you post here'
+        },
+        dialogPage: {
+            dialogData: [
+                {id: 1, name: "Andrey"},
+                {id: 2, name: "Matvey"},
+                {id: 3, name: "Olga"}
+            ],
+            messageData:[
+                {id: 1, message:"Hello, how are you?"},
+                {id: 2, message: "Hi"},
+                {id: 3, message: "Privet!"}
+              ],
+            newMessageText: "enter here your message"
+        }
+             
     },
-    dialogPage: {
-        dialogData: [
-            {id: 1, name: "Andrey"},
-            {id: 2, name: "Matvey"},
-            {id: 3, name: "Olga"}
-        ],
-        messageData:[
-            {id: 1, message:"Hello, how are you?"},
-            {id: 2, message: "Hi"},
-            {id: 3, message: "Privet!"}
-          ],
-        newMessageText: "enter here your message"
+
+    getState() {
+        return this._state;
+    },
+
+    _callObserver() {
+        console.log("test");
+    },
+
+    addPost() {
+        let newPost = {
+            id: 3,
+            message: this._state.profilePage.newPostText,
+            likes: "0"
+        };
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostText = "";
+        this._callObserver(this._state);
+    },
+
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callObserver(this._state);
+    },
+
+    addMessage() {
+        let newMessage ={
+            id: 4,
+            message: this._state.dialogPage.newMessageText
+        };
+        this._state.dialogPage.messageData.push(newMessage);
+        this._state.dialogPage.newMessageText ="";
+        this._callObserver(this._state);
+    },
+
+    updateNewMessageText(text) {
+        this._state.dialogPage.newMessageText=text;
+        this._callObserver(this._state);
+    },
+
+    subscribe(observer) {
+        this._callObserver = observer;
     }
-         
-}
 
-export let addPost = () =>{
-    let newPost = {
-        id: 3,
-        message: state.profilePage.newPostText,
-        likes: "0"
-    };
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostText = "";
-    renderEntireDoc(state);
-}
-export let updateNewPostText = (newText) =>{
-    state.profilePage.newPostText = newText;
-    renderEntireDoc(state);
-}
-
-export let addMessage = () =>{
-    let newMessage ={
-        id: 4,
-        message: state.dialogPage.newMessageText
-    };
-    state.dialogPage.messageData.push(newMessage);
-    state.dialogPage.newMessageText ="";
-    renderEntireDoc(state);
-}
-
-export let updateNewMessageText = (text)=> {
-    state.dialogPage.newMessageText=text;
-    renderEntireDoc(state);
 }
 
 
 
 
-export default state;
+
+
+export default store;
 
