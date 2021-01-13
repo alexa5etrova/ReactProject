980fc444e2001ea495a56245102e7b02af24fc9a
unknown <aleksa.5etrova@gmail.com>
2021-01-13 15:27:21 +0300

    update profile info style

--- src/components/Profile/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo.jsx
@@ -8,15 +8,30 @@ import cross from './../../assets/images/tickCross/cross.svg';
 
 const ProfileInfo = (props) => {
     return <div className={s.descriptionBlock}>
+        <div className={s.imgContainer}>
             <img src={props.profile.photos.large} />
-            <div>{props.profile.fullName}</div>
-            <div>{props.profile.aboutMe}</div>
-            <div>Looking for a job: {props.profile.lookingForAJob ? <img className={s.icon} src={tick} alt='tick icon'/> : <img className={s.icon} src={cross} alt='cross icon'/>}
-            </div>
-            <SocialMediaIcons contacts={props.profile.contacts}/>
         </div>
 
-    
+        <div className={s.infoContainer}>
+            <div className={s.userName}>{props.profile.fullName}</div>
+            <div className={s.aboutMe}>{props.profile.aboutMe}</div>
+            <div className={s.lookingForAJob}>
+                <div className={s.requireJob}>{'Looking for a job:'}</div>
+                <div className={s.crossTick}> {
+                    props.profile.lookingForAJob
+                        ? <img className={s.icon} src={tick} alt='tick icon' />
+                        : <img className={s.icon} src={cross} alt='cross icon' />
+                }
+                </div>
+                 
+            </div>
+            <SocialMediaIcons className={s.smContainer} contacts={props.profile.contacts} />
+        </div>
+
+
+    </div>
+
+
 }
 
 export default ProfileInfo;
