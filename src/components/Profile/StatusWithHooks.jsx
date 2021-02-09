990d1ec2bbadf5f02a6c4ef0c47449e8605f12be
Alexa <aleksa.5etrova@gmail.com>
2021-02-09 19:37:57 +0300

    useEffect

--- src/components/Profile/StatusWithHooks.jsx
+++ src/components/Profile/StatusWithHooks.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const StatusWithHooks = (props) => {
@@ -6,6 +6,11 @@ const StatusWithHooks = (props) => {
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
      
+
+    useEffect(()=>{
+        setStatus(props.status);
+    }, [props.status]);
+
     const openEditMode =()=>{
         setEditMode(true);
     }
